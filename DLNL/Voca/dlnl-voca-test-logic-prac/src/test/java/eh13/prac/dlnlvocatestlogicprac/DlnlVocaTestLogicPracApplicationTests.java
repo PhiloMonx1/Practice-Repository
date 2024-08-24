3eae9ab3486f5bf09bd6c8990943ccd056ec4c99
EH13\philo <philomonx1@gmail.com>
2024-08-24 22:17:29 +0900

    Init : [🏢DLNL-'보카 테스트 로직'] 프로젝트 초기화
    *feat : 프로젝트 생성
    *docs : 프로젝트 ReadME 작성

---
+++ DLNL/Voca/dlnl-voca-test-logic-prac/src/test/java/eh13/prac/dlnlvocatestlogicprac/DlnlVocaTestLogicPracApplicationTests.java
@@ -0,0 +1,13 @@
+package eh13.prac.dlnlvocatestlogicprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DlnlVocaTestLogicPracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
